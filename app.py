3f18f27e0cdd5bfa7e7412d21bd63fd7fbcabd7b
DaehuiHan <79817611+DaehuiHan@users.noreply.github.com>
2021-05-09 11:01:07 +0900

    GET 연습하기

--- app.py
+++ app.py
@@ -32,12 +32,16 @@ def write_review():
     # FIXME: POST 3. 이게 완료되면 어떤 동작을 해줄지 return에 적어준다.
     return jsonify({'msg': '저장 완료!'})
 
-
+# FIXME: GET 1. 일단 어떤 형태로 되어 있는지 확인해 본다.
 @app.route('/review', methods=['GET'])
 def read_reviews():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg': '이 요청은 GET!'})
+    # FIXME: GET 2. 클라이언트로부터 받는 데이터가 없기 떄문에 데이터를 받는 부분은 없애준다.
+    # sample_receive = request.args.get('sample_give')
+    # print(sample_receive)
+
+    # FIXME: GET 3. 여기서 GET요청이 할 일은 db에 있는 내용을 가져서 변수에 담고 그걸 클라이언트로 보내준다.
+    reviews = list(db.bookreview.find({}, {'_id': False}))
+    return jsonify({'all_reviews': reviews})
 
 
 if __name__ == '__main__':
